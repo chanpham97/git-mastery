f49a16379c6455763bc561adb80bc98272f0440d
Chan Pham <iamchanpham@gmail.com>
2024-11-07 16:51:53 +0000

    mastery instructions

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# git-mastery
+# Git Mastery
+Follow the instructions and commit your changes after each section.
+You are graded on the number of commits and commit messages. 
+
+## Section 1
+Answer the first 3 questions in index.js and commit.
+
+## Section 2
+Create a new file called copy.js and copy the content of index.js over. Commit this file.
+
+## Section 3
+Go into index.js and answer the rest of the questions. Commit this change.
+
+Lastly, push your code!
---
+++ index.js
@@ -0,0 +1,20 @@
+// Replace the ... with answers to the questions
+// Don't overthink the answers, anything is fine!
+// SECTION 1
+console.log("Q1. If you had to give a TED talk on anything, what would it be?")
+console.log("...")
+
+console.log("Q2. What's the last show/movie you watched that you would recommend?")
+console.log("...")
+
+console.log("Q3. What is an activity you do to relax or destress?")
+console.log("...")
+// Commit here! Reference README.md for section 2
+
+// SECTION 3
+console.log("Q4. What's something you're really proud of but don't get to talk about often?")
+console.log("...")
+
+console.log("Q5. If you could travel anywhere, where would you go?")
+console.log("...")
+// Commit here!
